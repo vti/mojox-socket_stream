25780975ce7699dc520afd0ba2a78713e71bda04
vti <viacheslav.t@gmail.com>
2010-01-28 11:38:41 +0100

    Added redis shorcuts

--- lib/MojoX/SocketStream/Redis.pm
+++ lib/MojoX/SocketStream/Redis.pm
@@ -7,6 +7,21 @@ use base 'MojoX::SocketStream';
 
 use MojoX::SocketStream::Redis::Message;
 
+our $AUTOLOAD;
+
 sub new_message { MojoX::SocketStream::Redis::Message->new }
+
+sub AUTOLOAD {
+    my $self = shift;
+    my ($method, @params) = @_;
+
+    return if $method =~ /^[A-Z]+?$/;
+    return if $method =~ /^_/;
+    return if $method =~ /(?:\:*?)DESTROY$/;
+
+    $method =~ s/^.+:://;
+
+    $self->send($method => @_);
+}
 
 1;
